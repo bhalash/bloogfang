9c44bfb6181dfabcbe482abff9a69104e1ddea36
Mark Grealish <mark@bhalash.com>
2026-03-17 19:53:44 +0000

    feat(users): finalize user footer style
    - Reduce text size, to not overshadow the post body.
    - Finalize + fix up layout.

--- src/lib/components/user.tsx
+++ src/lib/components/user.tsx
@@ -3,6 +3,21 @@ import { User, Post, fetchUser } from '@/lib/api';
 
 import '@/styles/user.css';
 
+/**
+ * Wordpress sends down the bio as a plain block of text. This is a Good Enough
+ * ghetto rendering.
+ */
+export function renderUserBio(bio: string) {
+  return bio
+    .split('\n')
+    .filter(text => !!text.trim())
+    .map(text => (
+      <p
+        key={text} dangerouslySetInnerHTML={{  __html: text }}
+      />
+    ));
+}
+
 export async function UserInfo({ post }: { post: Post }) {
   const user: User | undefined = await fetchUser(post.author);
 
@@ -11,17 +26,19 @@ export async function UserInfo({ post }: { post: Post }) {
   }
 
   return (
-    <footer className='flex user-footer justify-center'>
-      <div className='flex user-footer-content gap-4 max-w-4xl'>
-        <Image
-          alt={user.name}
-          src={user.avatar_urls['96']}
-          width={150}
-          height={150}
-        />
-        <div className='user-footer-bio flex flex-col'>
+    <footer className='user-footer p-8 mt-4 mb-4 max-w-4xl'>
+      <div className='inner flex justify-center gap-4'>
+        <figure className='mt-4'>
+          <Image
+            alt={user.name}
+            src={user.avatar_urls['96']}
+            width={96}
+            height={96}
+          />
+        </figure>
+        <div className='bio gap-3 flex flex-col'>
           <h4 className='font-bold text-3xl'>{user.name}</h4>
-          <p dangerouslySetInnerHTML={{ __html: user.description }} />
+          {renderUserBio(user.description)}
         </div>
       </div>
     </footer>
