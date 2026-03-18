8fb922db9a156c3233b9014de0e2ebc0bb749c93
Mark Grealish <mark@bhalash.com>
2026-03-18 23:34:49 +0000

    chore(user): pare down user bio parsing

--- src/lib/components/user.tsx
+++ src/lib/components/user.tsx
@@ -3,27 +3,17 @@ import { User, Post, fetchUser } from '@/lib/api';
 
 import '@/styles/user.css';
 
-/**
- * Wordpress sends down the bio as a plain block of text. This is a Good Enough
- * ghetto rendering.
- */
-export function renderUserBio(bio: string) {
-  return bio
-    .split('\n')
-    .filter(text => !!text.trim())
-    .map(text => (
-      <p
-        key={text} dangerouslySetInnerHTML={{  __html: text }}
-      />
-    ));
-}
-
 export async function UserInfo({ post }: { post: Post }) {
   const user: User | undefined = await fetchUser(post.author);
 
   if (!user) {
     return null;
   }
+
+  const bio = user.description
+    .split('\r\n')
+    .filter(Boolean)
+    .map((text) => <p key={text}>{text}</p>);
 
   return (
     <footer className='user-footer p-8 mt-4 mb-4 max-w-4xl skew-backward rounded-lg shadow-lg'>
@@ -39,7 +29,7 @@ export async function UserInfo({ post }: { post: Post }) {
         </figure>
         <div className='gap-3 flex flex-col'>
           <h4 className='font-bold text-3xl'>{user.name}</h4>
-          {renderUserBio(user.description)}
+          {bio}
         </div>
       </div>
     </footer>
